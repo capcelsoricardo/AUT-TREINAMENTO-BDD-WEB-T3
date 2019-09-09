6eebd039e0acfe3ce52efb0577030896228a15bc
Celso Ferreira <capcelsoricardo@gmail.com>
2019-09-09 09:14:17 -0300

    4. Encontro

--- src/test/java/curso/treinamento/steps/LoginSteps.java
+++ src/test/java/curso/treinamento/steps/LoginSteps.java
@@ -2,6 +2,8 @@ package curso.treinamento.steps;
 
 import java.io.File;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,19 +23,27 @@ public class LoginSteps {
 		
 		driver = new ChromeDriver();
 		
-		driver.get("https://www.phptravels.net/admin");
-		
-		
-		
+		driver.get("https://www.phptravels.net/admin");		
 	}
 
 	@Quando("faço login com o usuário {string} e senha {string}")
-	public void faço_login_com_o_usuário_e_senha(String string, String string2) {
+	public void faço_login_com_o_usuário_e_senha(String user, String pass) {
 
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(user);
+		
+		driver.findElement(By.name("password")).sendKeys(pass);
+		
+		driver.findElement(By.xpath("//span[text()='Login']")).click();
+		
 	}
 
 	@Então("sou autenticado com sucesso")
-	public void sou_autenticado_com_sucesso() {
+	public void sou_autenticado_com_sucesso() throws InterruptedException {
+		
+		Thread.sleep(3000);
+		
+		Assert.assertTrue(driver.findElement(
+				By.xpath("//a[@href='https://www.phptravels.net/admin/logout']")).isDisplayed());
 	
 	}
 
--- target/test-classes/features/login.feature
+++ target/test-classes/features/login.feature
@@ -4,5 +4,5 @@ Funcionalidade: Login
 
 	Cenário: Realizar login com sucesso
 		Dado que eu deseje realizar login
-		Quando faço login com o usuário "xpto" e senha "senha"
+		Quando faço login com o usuário "admin@phptravels.com" e senha "demoadmin"
 		Então sou autenticado com sucesso 
